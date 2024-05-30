3f3b2186f6b4c8807eac34e07e7e08f2c9eaaa4c
Eleazarguitar18 <eleazarguitar18@gmail.com>
2024-05-29 22:50:26 -0400

    datos primitivos

--- Datos_priminitivos.js
+++ Datos_priminitivos.js
@@ -3,3 +3,21 @@ let Bolivar = 11;
 let Tigre = 11;
 let messi = 1;
 let JuegaMessi = true;
+let palabras = "Algo tenia que venir aqui";
+function jugar(equipo1, equipo2, JuegaMessi) {
+    let motivo = "";
+    if (JuegaMessi) {
+        equipo1 += messi;
+        motivo = "porque juega messi";
+    }
+    if (equipo1 > equipo2) {
+        console.log(`Gana Bolivar ${motivo}`);
+    }
+    if (equipo1 == equipo2) {
+        console.log(`Empatan`);
+    }
+    if (equipo1 < equipo2) {
+        console.log(`Gana Tigre`);
+    }
+}
+jugar(Bolivar, Tigre, JuegaMessi);
--- Datos_priminitivos.ts
+++ Datos_priminitivos.ts
@@ -2,3 +2,23 @@ let Bolivar: number = 11;
 let Tigre: number = 11;
 let messi: number = 1;
 let JuegaMessi: boolean = true;
+
+let palabras: string = "Algo tenia que venir aqui";
+
+function jugar(equipo1: number, equipo2: number, JuegaMessi: boolean) {
+  let motivo: string = "";
+  if (JuegaMessi) {
+    equipo1 += messi;
+    motivo = "porque juega messi";
+  }
+  if (equipo1 > equipo2) {
+    console.log(`Gana Bolivar ${motivo}`);
+  }
+  if (equipo1 == equipo2) {
+    console.log(`Empatan`);
+  }
+  if (equipo1 < equipo2) {
+    console.log(`Gana Tigre`);
+  }
+}
+jugar(Bolivar, Tigre, JuegaMessi);
